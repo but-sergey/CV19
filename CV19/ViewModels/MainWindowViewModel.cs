650701ce391ead57c4767f764a88b727cf124f24
but-sergey <but-sergey@bk.ru>
2020-05-26 13:29:11 +0300

    Тестирование работы модели-представления  главного окна

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using CV19.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CV19.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Window title
+
+        private string _Title = "Анализ статистики COVID-19";
+
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+            //{
+            //    if (Equals(_title, value)) return;
+            //    _title = value;
+            //    OnPropertyChanged();
+            //}
+        }
+
+        #endregion
     }
 }
